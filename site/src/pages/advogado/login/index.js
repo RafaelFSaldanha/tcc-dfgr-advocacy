6baf322ff6c1e74b9438f061377f9587c9cf35c5
dieguitozz <diegolenda2006@gmail.com>
2022-09-17 00:08:19 -0300

    login page 100%

--- site/src/pages/advogado/login/index.js
+++ site/src/pages/advogado/login/index.js
@@ -1,7 +1,6 @@
 import './index.scss'
 import '../../common/common.scss'
 import logoDourada from '../../../assets/logodourada.svg'
-import logoGoogle from '../../../assets/logogoogle.svg'
 
 export default function LoginPage() {
     return(
@@ -9,14 +8,18 @@ export default function LoginPage() {
             <div className='div-bg-main'>
                 <img className='logo' src={logoDourada}/>
                 <div className='div-bg-input'>
-                    <p>Email</p>
-                    <input placeholder='Insira seu email'/>
-                    <p>Senha</p>
-                    <input placeholder='********'/>
+                   <div className='input-email'>
+                    <p>Email<span> *</span></p>
+                    <input />
+                    </div>
+                    <div className='input-senha'>
+                    <p>Senha <span> *</span></p>
+                    <input />
+                    </div>
                 </div>
                 <div className='div-bg-button'>
                     <button className='entrar-button'>Entrar</button>
-                    <button className='google-button'> <img src={logoGoogle}/>Sign in with Google</button>
+                    
                 </div>
                 <p></p>
             </div>
