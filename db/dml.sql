0aac2dc03f19052deca9ca0a2ebc131dd4408447
diegutiozz <diegolenda2006@gmail.com>
2022-09-21 17:25:04 -0300

    barra lateral

--- db/dml.sql
+++ db/dml.sql
@@ -1,8 +1,9 @@
 insert into tb_cliente(nm_cliente, ds_email, ds_senha)
 	values('Kalel Rodrigues da Silva', 'zoroplay1220@gmail.com', 'kalel123');
-
-insert into tb_advogado(id_area, nm_advogado, ds_email, ds_senha, ds_localizacao, ds_oab, nr_telefone)
-	values(1, "Diego Silva Dias", "advogado@advogado.com", "1234", "São Paulo", "SP123456", "(11)982776312");
     
 insert into tb_area_atuacao(nm_area)
 	values("Ambiental");	
+    
+insert into tb_advogado(id_area, nm_advogado, ds_email, ds_senha, ds_localizacao, ds_oab, nr_telefone)
+	values(1, "Diego Silva Dias", "advogado@advogado.com", "1234", "São Paulo", "SP123456", "(11)982776312");
+    
