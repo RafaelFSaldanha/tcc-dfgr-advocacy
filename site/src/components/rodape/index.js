5d80186be6a4259545c306daf317e66cad1a1e2d
dieguitozz <diegolenda2006@gmail.com>
2022-10-10 22:55:48 -0300

    just kidding on landing page

--- site/src/components/rodape/index.js
+++ site/src/components/rodape/index.js
@@ -9,18 +9,18 @@ export default function Rodape(){
                 <img className='logo' src='/assets/images/logodourada.svg' alt='logo' />
                 <p>Há mais de 30 anos trabalhando para garantir o seu direito com responsabilidade e ética.</p>
                 <div className='div-social'>
-                    <img src='/assets/images/facebook.png' alt='facebook' />
-                    <img src='/assets/images/twitter.png' alt='twitter' />
-                    <img src='/assets/images/instagram.png' alt='instagram' />
+                    <img src='/assets/images/facebook2.svg' alt='facebook' />
+                    <img src='/assets/images/twitter2.svg' alt='twitter' />
+                    <img src='/assets/images/instagram2.svg' alt='instagram' />
                 </div>
             </div>
             <div className='div2'>
                 <h2> Informações de Contato </h2>
-                <h3> Email </h3>
+                <h3> <img src="/assets/images/email.svg" /> Email </h3>
                 <p> dfgr_advogados&associados@gmail.com.br </p>
                 <h3> Telefones: </h3>
-                <p> (11) 5921-1924 </p>
-                <p> (11) 5920-3596 </p>
+                <p> <img src="/assets/images/telefone.svg" /> (11) 5921-1924 </p>
+                <p> <img src="/assets/images/telefone.svg" /> (11) 5920-3596 </p>
             </div>
         </footer>
     )
